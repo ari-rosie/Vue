359201614e2c49b8eab5474d37f28dfc0320bb4e
ari-rosie <ariane@ghalleb.com>
2020-11-04 15:37:53 -0500

    wed 15h30

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <img
+    class="menu-item__image"
+    :src="imgSrc"
+    :alt="imgAlt"
+    />
+    <div>
+        <h3>{{ name }}</h3>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <div>
+            <label for="add-item-quantity"
+            >Quantity: {{ quantity }}</label
+            >
+            <input
+            v-model.number="quantity"
+            id="add-item-quantity"
+            type="number"
+            />
+            <button @click="addToShoppingCart(quantity)">
+            Add to Shopping Cart
+            </button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'MenuItem',
+    props:[ 'addToShoppingCart', 'imgSrc', 'imgAlt', 'name', 'inStock', 'quantity']
+}
+</script>
